bf2c045905f25fc48f073c4008af7cffbd79e9db
jimmy <foodsksms@gmail.com>
2018-05-11 15:02:31 +0900

    profile 조회 구현중 url 이동 가능

--- src/main/java/com/qna2/qna2/web/UserController.java
+++ src/main/java/com/qna2/qna2/web/UserController.java
@@ -3,7 +3,9 @@ package com.qna2.qna2.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,14 +18,20 @@ public class UserController {
     public String create(User usr) {
         System.out.println("User : " + usr);
         users.add(usr);
-        return "redirect:/list";
+        return "redirect:/users";
         // redirect:/url로 이동을 하게된다.
         // static에 있는 index.html이 아니라, templates에 있는 index.html을 호출을 한다.
     }
 
-    @GetMapping("/list")
+    @GetMapping("/users")
     public String list(Model model) {
         model.addAttribute("users", users);
         return "list";
     }
-}
+
+    @GetMapping("/users/{userId}")
+    public String getUser(@PathVariable String userId ,Model model) {
+            model.addAttribute("user", userId);
+            return "profile";
+        }
+    }
