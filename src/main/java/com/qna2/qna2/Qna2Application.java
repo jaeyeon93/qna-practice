01cb93dd8073665acddd3d08fd7c82b9d75a9a25
jimmy <foodsksms@gmail.com>
2018-05-11 11:52:26 +0900

    second qna start

---
+++ src/main/java/com/qna2/qna2/Qna2Application.java
@@ -0,0 +1,12 @@
+package com.qna2.qna2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qna2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qna2Application.class, args);
+	}
+}
