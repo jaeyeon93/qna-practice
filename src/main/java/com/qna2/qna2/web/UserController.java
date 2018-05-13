cdaba149ebe5a460f03099148c607d5c66850a95
JaeyeonKIM <foodsksms@gmail.com>
2018-05-14 00:46:52 +0900

    반복주기2 마무리

--- src/main/java/com/qna2/qna2/web/UserController.java
+++ src/main/java/com/qna2/qna2/web/UserController.java
@@ -6,10 +6,7 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,7 +48,7 @@ public class UserController {
         return "/user/updateForm";
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String update(@PathVariable Long id, User newUser) {
         User user = userRepository.findOne(id);
         user.update(newUser);
