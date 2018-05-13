550ca55f1855b322dc52a739546b9235c7231a88
JaeyeonKIM <foodsksms@gmail.com>
2018-05-14 00:19:53 +0900

    qna 조회 및 질문가능

--- src/main/java/com/qna2/qna2/web/QuestionController.java
+++ src/main/java/com/qna2/qna2/web/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -29,6 +30,12 @@ public class QuestionController {
     @GetMapping("")
     public String list(Model model) {
         model.addAttribute("questions", qnARepository.findAll());
-        return "qna/show";
+        return "qna/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getQnA(@PathVariable Long id, Model model) {
+        model.addAttribute("questions", qnARepository.findOne(id));
+        return "/qna/show";
     }
 }
