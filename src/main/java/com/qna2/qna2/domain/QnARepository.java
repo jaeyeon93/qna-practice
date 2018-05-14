48af341ad1fb38dfd94ecaa4385f1125cbcedd2f
jimmy <foodsksms@gmail.com>
2018-05-14 14:36:21 +0900

    file moved to another project

--- src/main/java/com/qna2/qna2/domain/QnARepository.java
+++
@@ -1,6 +0,0 @@
-package com.qna2.qna2.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QnARepository extends JpaRepository<QnA, Long> {
-}
--- src/main/java/com/qna2/qna2/domain/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.qna2.qna2.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
